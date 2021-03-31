82965ecf8f23a8b3eac89344ff9bf965b1c9e7c2
Devan Mallory <32146940+dman82499@users.noreply.github.com>
2021-03-31 16:14:36 -0400

    added democracy voting function

--- bot2.py
+++ bot2.py
@@ -8,10 +8,12 @@ description = "free democracy bot 2.0 : Starting up"
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='@', description=description, intents=intents)
+bot = commands.Bot(command_prefix='-', description=description, intents=intents)
 
 past_kicked_users = [] 
-
+is_voting = False
+yes_votes = 0
+no_votes = 0
 
 @bot.event
 async def on_ready():
@@ -19,6 +21,7 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+
 
 async def process_vote():
     await asyncio.sleep()
@@ -47,15 +50,43 @@ async def choose(ctx, *choices: str):
 
 
 @bot.command()
-async def votekick(ctx, user:discord.user):
-    if type(user) != discord.user:
+async def vote(ctx, msg:str):
+    global is_voting
+    bot_vote = is_voting
+    global yes_votes
+    global no_votes
+    if is_voting:
+        if msg == 'yes':
+            await ctx.send(ctx.author.id + "has voted yes!")
+            yes_votes += 1
+        if msg == 'no':
+            await ctx.send(ctx.author.id + " has voted no!")
+            no_votes += 1
+    else:
+        await ctx.send("you can't vote right now!")
+
+
+@bot.command()
+async def votekick(ctx, user:discord.User):
+    global is_voting
+    global past_kicked_users
+    global yes_votes
+    global no_votes
+    if type(user) != discord.User:
         await ctx.send("you need to select someone to vote kick first!")
     else:
         past_kicked_users.append(user)
-        await  ctx.send("starting votekick for user:", user)
-        await ctx.send("type \"voteyes\" or \"voteno\" to cast your vote!\n if the majority of votes are in favor, the defendent shall be kicked")
-        await asyncio.sleep(10)
+        is_voting = True
+        await  ctx.send("starting votekick for user:" + str(user))
+        await ctx.send("type \"vote yes\" or \"vote no\" to cast your vote!\n if the majority of votes are in favor, the defendent shall be kicked")
+        await asyncio.sleep(15)
         await ctx.send("voting is finished!")
+        is_voting = False
+        await ctx.send("total yes votes: " + str(yes_votes) + "\ntotal no votes: " + str(no_votes))
+        if yes_votes > no_votes:
+            await ctx.send("by law of the people of" + ctx.message.guild.name + ", the defendant is guilty! Prepare to be kicked. \n You have ten seconds left to say your last words.")
+        yes_votes = 0
+        no_votes = 0
 
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
@@ -65,4 +96,4 @@ async def repeat(ctx, times: int, content='repeating...'):
 
 
 
-bot.run('token')
+bot.run('Njk4NDY3MDE4MjE0NzM1OTEy.XpGQGg.eMAh_bYPd10SNU4zFaAFYfwDHfA')
