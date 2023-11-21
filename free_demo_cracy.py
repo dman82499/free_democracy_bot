70d5dd3ca995457bb29f51386ed17a8052ffc9cf
Devan Mallory <devan.mallory@yahoo.com>
2023-11-21 03:05:41 -0500

    created knuckles rates your meme application and updated existing prompts

--- free_demo_cracy.py
+++ free_demo_cracy.py
@@ -3,12 +3,18 @@ import discord
 from discord.ext import commands
 import random
 import asyncio
+import knuckles_memelist
+try:
+    import key
+except:
+    print("You need to insert a custom key!")
 
 description = "free democracy bot 2.0 : Starting up"
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.members = True
 
-bot = commands.Bot(command_prefix='-', description=description, intents=intents)
+bot = commands.Bot(command_prefix='/', description=description, intents=intents)
+
 
 past_kicked_users = {}
 voted_users = []
@@ -26,9 +32,12 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+#@bot.event
+#async def on_message(message):
+#    print(message.content)
 
 async def process_vote():
-    await asyncio.sleep()
+    await asyncio.sleep(2)
 
 @bot.command()
 async def add(ctx, left: int, right: int):
@@ -81,20 +90,20 @@ async def on_reaction_add(reaction, user):
     global is_voting
     global yes_votes
     global no_votes
-    global channel
+    
     emoji = reaction.emoji
     #this will eventually be changed to a fixed channel
-    cxt = channel
+
     if is_voting:
         if user in voted_users:
-            await cxt.send(user.mention + ", you have already voted and cannot vote twice!")
+            await bot.get_channel(id=1).send(user.mention + ", you have already voted and cannot vote twice!")
         else:
             if emoji == "✅":
-                await cxt.send(user.mention +" voted yes!")
+                await bot.get_channel(id=1).send(user.mention +" voted yes!")
                 yes_votes += 1
             if emoji == "❌":
                 no_votes += 1
-                await cxt.send(user.mention+ " voted no!")
+                #await cxt.send(user.mention+ " voted no!")
             voted_users.append(user)
 
 
@@ -157,22 +166,41 @@ async def votekick(ctx, user:discord.Member):
             no_votes = 0
 
 
+@votekick.error
+async def votekick_error(ctx, message):
+    await ctx.send("you must pick a valid user to kick!")
+
 @bot.command()
 async def die(ctx):
+    print("die command executed")
     await ctx.send("k bitch :(")
-    quit()
 
-async def rate_meme(ctx):
-    rand_number = random.randint(0, 4)
-    file_name = r"c:\location\of\the_file_to\send" + str(rand_number)+ ".mp4"
-    area = ctx.message.channel
-    await ctx.send(file=discord.File(file_name))
+@bot.command()
+async def rate_meme(ctx, message:discord.Attachment=None):
+    #print(message)
+    #if message is None:
+   #     await ctx.send("You need to send a valid meme!")
+    #else:
+    embed_msg = discord.Embed(description="rating meme for:")
+    embed_msg.set_image(url=message.url)
+    rand_number = random.randint(0, len(knuckles_memelist.memes)-1)
+    await ctx.send(f"rating meme for:", embed=embed_msg)
+    await asyncio.sleep(2)
+    await ctx.send(knuckles_memelist.memes[rand_number])
+@rate_meme.error
+async def rate_meme_error(ctx, error):
+    await ctx.send("You need to send a valid meme!")
+
+
+    #file_name = r"c:\location\of\the_file_to\send" + str(rand_number)+ ".mp4"
+    #area = ctx.message.channel
+    #await ctx.send(file=discord.File(file_name))
 
 
 
 @bot.command()
 async def fortnite(ctx, msg:int):
-    lmao = '''@everyone AMONGUS Among Us[c] is an online multiplayer social deduction game developed and published by American game studio Innersloth. It was released on iOS and Android devices in June 2018 and on Windows in November 2018, 
+    lmao = '''AMONGUS Among Us[c] is an online multiplayer social deduction game developed and published by American game studio Innersloth. It was released on iOS and Android devices in June 2018 and on Windows in November 2018, 
     featuring cross-platform play between these platforms.[4] The game was also ported for the Nintendo Switch in December 2020, and 
     has planned releases for the Xbox One and Xbox Series X and Series S in 2021. The game was inspired by the party game Mafia and the science fiction horror film The Thing, and since the release of its first map, The Skeld, other maps were added into the game. A fourth map, "The Airship", is set to be released in March 2021.
 
@@ -186,8 +214,16 @@ be Impostors may be eliminated via a plurality vote, which any player may initia
     else:
         await ctx.send(lmao)
 
+@fortnite.error
+async def fortnite_error(ctx, error):
+    lmao = '''AMONGUS Among Us[c] is an online multiplayer social deduction game developed and published by American game studio Innersloth. It was released on iOS and Android devices in June 2018 and on Windows in November 2018, 
+        featuring cross-platform play between these platforms.[4] The game was also ported for the Nintendo Switch in December 2020, and 
+        has planned releases for the Xbox One and Xbox Series X and Series S in 2021. The game was inspired by the party game Mafia and the science fiction horror film The Thing, and since the release of its first map, The Skeld, other maps were added into the game. A fourth map, "The Airship", is set to be released in March 2021.
 
-
+    The game takes place in a space-themed setting, in which players each take on one of two roles, most being Crewmates, and a predetermined number being Impostors.[d] The goal of the Crewmates is to identify the Impostors, 
+    eliminate them, and complete tasks around the map; the Impostors' goal is to covertly sabotage and kill the Crewmates before they complete all of their tasks. Players suspected to 
+    be Impostors may be eliminated via a plurality vote, which any player may initiate by calling an emergency meeting (except during a crisis) or reporting a dead body. Crewmates win if all Impostors are eliminated or all tasks are completed whereas Impostors win if there is an equal number of Impostors and Crewmates, or if a critical sabotage goes unresolved.'''
+    await ctx.send(lmao)
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
     """Repeats a message multiple times."""
@@ -209,5 +245,5 @@ async def set_channel(ctx, a:str):
     channel = ctx.message.channel
     print("lol")
 
-
-bot.run('')
+# You will need to insert a custom key here if you would like to host
+bot.run(key.token)
